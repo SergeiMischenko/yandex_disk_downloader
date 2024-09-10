064ed744bc2897258905b81c0313039b017411bc
Sergey <main0serg@gmail.com>
2024-09-10 14:02:39 +0600

    added cache

--- disk_app/views.py
+++ disk_app/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.shortcuts import render
 
 from disk_app.api import get_public_files
@@ -14,12 +15,17 @@ def file_list_view(request):
         form = PublicKeyForm(request.POST)
         if form.is_valid():
             public_key = form.cleaned_data['public_key']
-            data = get_public_files(public_key)
-            #  Проверка на наличие файлов в публичной ссылке
-            if data and '_embedded' in data:
-                files = data['_embedded']['items']
-
-                # Если выбран фильтр, фильтруем файлы по типу
-                if file_type:
-                    files = [f for f in files if get_file_type(f['name']) == file_type]
+            cache_key = f'yandex_disk_files_{public_key}'
+            # Попробуем получить данные из кэша
+            files = cache.get(cache_key)
+            if not files:
+                data = get_public_files(public_key)
+                #  Проверка на наличие файлов в публичной ссылке
+                if data and '_embedded' in data:
+                    files = data['_embedded']['items']
+                    # Сохраняем список файлов в кэш на 10 минут
+                    cache.set(cache_key, files, timeout=600)
+            # Если выбран фильтр, фильтруем файлы по типу
+            if file_type:
+                files = [f for f in files if get_file_type(f['name']) == file_type]
     return render(request, 'disk_app/file_list.html', {'form': form, 'files': files, 'file_type': file_type})
