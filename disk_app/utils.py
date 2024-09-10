b8f7325c4e3fe231c2181b76b4278f034bf37fe2
Sergey <main0serg@gmail.com>
2024-09-10 13:44:28 +0600

    added filters

---
+++ disk_app/utils.py
@@ -0,0 +1,19 @@
+def get_file_type(file_name: str):
+    extension = file_name.split('.')[-1].lower()
+    # Изображения
+    if extension in ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff']:
+        return 'image'
+    # Документы
+    elif extension in ['pdf', 'doc', 'docx', 'txt', 'odt', 'xls', 'xlsx', 'ppt', 'pptx']:
+        return 'document'
+    # Архивы
+    elif extension in ['zip', 'rar', '7z', 'tar', 'gz']:
+        return 'archive'
+    # Медиа файлы
+    elif extension in ['mp3', 'wav', 'aac', 'flac', 'mp4', 'mkv', 'avi', 'mov']:
+        return 'media'
+    # Исполняемые файлы
+    elif extension in ['exe', 'msi', 'bat', 'sh', 'bin']:
+        return 'executable'
+    # Другие типы файлов
+    return 'other'
--- disk_app/views.py
+++ disk_app/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 from disk_app.api import get_public_files
 from disk_app.forms import PublicKeyForm
+from disk_app.utils import get_file_type
 
 
 # Create your views here.
 def file_list_view(request):
     form = PublicKeyForm()
     files = None
+    file_type = request.POST.get('type')  # Получаем тип фильтра
     if request.method == 'POST':
         form = PublicKeyForm(request.POST)
         if form.is_valid():
@@ -16,4 +18,8 @@ def file_list_view(request):
             #  Проверка на наличие файлов в публичной ссылке
             if data and '_embedded' in data:
                 files = data['_embedded']['items']
-    return render(request, 'disk_app/file_list.html', {'form': form, 'files': files})
+
+                # Если выбран фильтр, фильтруем файлы по типу
+                if file_type:
+                    files = [f for f in files if get_file_type(f['name']) == file_type]
+    return render(request, 'disk_app/file_list.html', {'form': form, 'files': files, 'file_type': file_type})
